a9f071c62d142287ba019c7717cb5aab39ca4bb2
Kyle Burkholder <kbburkholder@gmail.com>
2014-11-03 23:31:47 -0500

    Initial Commit

---
+++ BinaryTree/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BinaryTree
+//
+//  Created by Kyle Burkholder on 11/3/14.
+//  Copyright (c) 2014 Kyle Burkholder. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
