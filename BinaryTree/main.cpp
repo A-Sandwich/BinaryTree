08057a26b48737d4aace4b51b69cfd699f2c0c93
Kyle Burkholder <kbburkholder@gmail.com>
2014-11-04 23:01:08 -0500

    added funciton for adding new node to tree.

--- BinaryTree/main.cpp
+++ BinaryTree/main.cpp
@@ -11,19 +11,48 @@
 using namespace std;
 
 struct node{
-    string name = "default";
+    int value = NULL;
+    int quantity = 1;
     node* leftNode = NULL;
     node* rightNode = NULL;
 };
+
+void positionAndInsertNode(node* root, int value);
+void findAndRemove(node* parent, int value);
 
 int main(int argc, const char * argv[]) {
     node* root = new node;
     node* left = new node;
     node* right = new node;
-    root->name = "root";
     
     
     std::cout << "Hello, World!\n" << endl;
-    cout << root->name << ", " << root->leftNode->name;
     return 0;
 }
+
+/*
+ * Traverses tree and inserts node or iterates count.
+ */
+void positionAndInsertNode(node* parent, node* newNode){
+    if(parent->value < newNode->value){
+        if(parent->leftNode == NULL)
+            parent->leftNode = newNode;
+        else
+            positionAndInsertNode(parent->leftNode, newNode);
+    }else if(parent->value > newNode->value){
+        if(parent->rightNode == NULL)
+            parent->rightNode = newNode;
+        else
+            positionAndInsertNode(parent->rightNode, newNode);
+    }else{
+        parent->quantity++;
+    }
+        
+}//end insertNode
+
+/*
+ * traverses tree and if the node exists it either decrements quantity or removes node.
+ */
+void findAndRemove(node* parent, int value){
+    
+}//end findAndRemove
