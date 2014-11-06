68b7f1e173704f2b3d822b4eb72c171dd3fdbcc3
Kyle Burkholder <kbburkholder@gmail.com>
2014-11-05 23:05:05 -0500

    Started remove function. Still need to finish and optimize/not repeate myself.

--- BinaryTree/main.cpp
+++ BinaryTree/main.cpp
@@ -15,6 +15,7 @@ struct node{
     int quantity = 1;
     node* leftNode = NULL;
     node* rightNode = NULL;
+    bool isRoot = false;
 };
 
 void positionAndInsertNode(node* root, int value);
@@ -29,6 +30,8 @@ int main(int argc, const char * argv[]) {
     std::cout << "Hello, World!\n" << endl;
     return 0;
 }
+
+
 
 /*
  * Traverses tree and inserts node or iterates count.
@@ -53,6 +56,22 @@ void positionAndInsertNode(node* parent, node* newNode){
 /*
  * traverses tree and if the node exists it either decrements quantity or removes node.
  */
-void findAndRemove(node* parent, int value){
+void findAndRemove(node* parent, node* currentNode, int value){
     
+    if(parent->value == value){
+        if(parent->isRoot && currentNode->isRoot){
+            if(parent->leftNode == NULL && parent->rightNode == NULL && currentNode->quantity <= 1){
+                parent->value = NULL;
+                parent->quantity = 0;
+            }else{
+                parent->quantity--;
+            }
+        }else{
+            
+        }
+    }else if(parent->value > value){
+        findAndRemove(currentNode, currentNode->leftNode, value);
+    }else{
+        findAndRemove(currentNode, currentNode->rightNode, value);
+    }
 }//end findAndRemove
